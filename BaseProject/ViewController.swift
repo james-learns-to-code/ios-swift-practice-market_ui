8ca33f8e53318b72cd929b53d1cb193f56adc6e4
James Lee <jameslee@goodeffect.com>
2019-06-15 04:04:32 +0900

    Create VC with view for comparing code with separated view logic.

--- BaseProject/ViewController.swift
+++ BaseProject/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction private func didTapButton() {
+    @IBAction private func didTapFirstButton() {
         presentVC()
     }
     private func presentVC() {
         let vc = StarwarsViewController()
+        present(vc, animated: true, completion: nil)
+    }
+ 
+    @IBAction private func didTapSecondButton() {
+        presentVCWithView()
+    }
+    private func presentVCWithView() {
+        let vc = StarwarsViewControllerWithView()
         present(vc, animated: true, completion: nil)
     }
 }
